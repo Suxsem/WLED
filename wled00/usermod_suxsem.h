3ca6dd720018b43ab6fba4c292dea3e923b28dc6
Roberta Giordano <rgiordano@alteanet.it>
2024-03-27 09:41:04 +0100

    SuxsemUsermod: added configurations

--- wled00/usermod_suxsem.h
+++ wled00/usermod_suxsem.h
@@ -5,12 +5,24 @@
 #define SUXSEM_AC_BTN_PIN             0
 #define SUXSEM_SHORT_MAX_MS         600
 #define SUXSEM_DEBOUNCE_THRESHOLD    50
-#define SUXSEM_PRESSED_LEVEL        LOW
+#define SUXSEM_INVERT_PRESSED     false
 #define SUXSEM_DIMMING_TICK_MS        8
 #define SUXSEM_FADEOUT_MS          2000
 
 class UsermodSuxsem : public Usermod {
   private:
+
+    static const char _name[];
+    static const char _enabled[];
+    unsigned short btnPin = SUXSEM_AC_BTN_PIN;
+    unsigned int shortMaxMs = SUXSEM_SHORT_MAX_MS;
+    unsigned int debounceMs = SUXSEM_DEBOUNCE_THRESHOLD;
+    bool invertPressed = SUXSEM_INVERT_PRESSED;
+    unsigned int dimmingTickMs = SUXSEM_DIMMING_TICK_MS;
+    unsigned int fadeoutMs = SUXSEM_FADEOUT_MS;
+    
+    bool enabled = false;
+
     unsigned long buttonPressedTime = 0;
     unsigned long lastDimmTime = 0;
     bool alreadyTurnedOn = false;
@@ -19,41 +31,44 @@ class UsermodSuxsem : public Usermod {
 
   public:
 
+    inline void enable(bool enable) { enabled = enable; }
+    inline bool isEnabled() { return enabled; }
+
     void setup() {
-      pinMode(SUXSEM_AC_BTN_PIN, INPUT);
+      pinMode(btnPin, INPUT);
     }
 
     void loop() {
-      if (strip.isUpdating()) return;
+      if (!enabled || strip.isUpdating()) return;
 
       unsigned long now = millis();
       unsigned long elapsed = now - buttonPressedTime;
 
-      if (buttonPressedTime == 0 && digitalRead(SUXSEM_AC_BTN_PIN) == SUXSEM_PRESSED_LEVEL) { //premo per la prima volta
+      if (buttonPressedTime == 0 && digitalRead(btnPin) != invertPressed) { //premo per la prima volta
         buttonPressedTime = now;
-      } else if (buttonPressedTime != 0 && elapsed > SUXSEM_DEBOUNCE_THRESHOLD && !alreadyTurnedOn && bri == 0) { //effettivamente premuto
+      } else if (buttonPressedTime != 0 && elapsed > debounceMs && !alreadyTurnedOn && bri == 0) { //effettivamente premuto
         //TURN ON
         jsonTransitionOnce = true;
         strip.setTransition(0);
         bri = 255;
         stateUpdated(CALL_MODE_BUTTON);
         alreadyTurnedOn = true;
-      } else if (buttonPressedTime != 0 && digitalRead(SUXSEM_AC_BTN_PIN) != SUXSEM_PRESSED_LEVEL) { //rilascio
-        if (!alreadyTurnedOn && elapsed <= SUXSEM_SHORT_MAX_MS && elapsed > SUXSEM_DEBOUNCE_THRESHOLD && bri != 0) {
+      } else if (buttonPressedTime != 0 && digitalRead(btnPin) == invertPressed) { //rilascio
+        if (!alreadyTurnedOn && elapsed <= shortMaxMs && elapsed > debounceMs && bri != 0) {
           jsonTransitionOnce = true;
-          strip.setTransition(SUXSEM_FADEOUT_MS);
+          strip.setTransition(fadeoutMs);
           bri = 0;
           stateUpdated(CALL_MODE_BUTTON);
         }
         dimming = false;
         alreadyTurnedOn = false;
         buttonPressedTime = 0;
-      } else if (buttonPressedTime != 0 && elapsed > SUXSEM_SHORT_MAX_MS && !dimming) { //continuo a premere
+      } else if (buttonPressedTime != 0 && elapsed > shortMaxMs && !dimming) { //continuo a premere
         //DIMMING
         dimming = true;
       }
 
-      if (dimming && (now - lastDimmTime) > SUXSEM_DIMMING_TICK_MS) {
+      if (dimming && (now - lastDimmTime) > dimmingTickMs) {
         if (bri == 255 && direzione == 1) {
           direzione = -1;
         } else if (bri <= 1 && direzione == -1) {
@@ -67,4 +82,33 @@ class UsermodSuxsem : public Usermod {
       }
 
     }
+
+    void addToConfig(JsonObject& root) override
+    {
+      JsonObject top = root.createNestedObject(FPSTR(_name));
+      top[FPSTR(_enabled)] = enabled;
+      top["btnPin"] = btnPin;
+      top["shortMaxMs"] = shortMaxMs;
+      top["debounceMs"] = debounceMs;
+      top["invertPressed"] = invertPressed;
+      top["dimmingTickMs"] = dimmingTickMs;
+      top["fadeoutMs"] = fadeoutMs;
+    }
+
+    bool readFromConfig(JsonObject& root) override
+    {
+      JsonObject top = root[FPSTR(_name)];
+      bool configComplete = !top.isNull();
+      configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled, false);
+      configComplete &= getJsonValue(top["btnPin"], fadeoutMs, SUXSEM_AC_BTN_PIN);
+      configComplete &= getJsonValue(top["shortMaxMs"], fadeoutMs, SUXSEM_SHORT_MAX_MS);
+      configComplete &= getJsonValue(top["debounceMs"], fadeoutMs, SUXSEM_DEBOUNCE_THRESHOLD);
+      configComplete &= getJsonValue(top["invertPressed"], fadeoutMs, SUXSEM_INVERT_PRESSED);
+      configComplete &= getJsonValue(top["dimmingTickMs"], fadeoutMs, SUXSEM_DIMMING_TICK_MS);
+      configComplete &= getJsonValue(top["fadeoutMs"], fadeoutMs, SUXSEM_FADEOUT_MS);
+      return configComplete;
+    }
 };
+
+const char UsermodSuxsem::_name[]    PROGMEM = "SuxsemUsermod";
+const char UsermodSuxsem::_enabled[] PROGMEM = "enabled";
